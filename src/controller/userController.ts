4816d6c4874e94a3f431a2402e73b63245edf725
Gustavo <gustavolopez819@gmail.com>
2026-06-18 13:07:57 -0500

    feat(users): add token validation

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,15 +1,36 @@
 import { Request, Response } from "express";
-import { supabase } from "../config/supabase";
+import { createClient } from "@supabase/supabase-js";
 
 export class User {
   static getUser = async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
+      const authHeader = req.headers.authorization;
 
       if (!id) {
         res.status(400).json({ error: "Id inválido" });
         return;
       }
+
+      const token = authHeader.replace("Bearer ", "");
+
+      if (!authHeader?.startsWith("Bearer ")) {
+        return res.status(401).json({
+          error: "No autorizado",
+        });
+      }
+
+      const supabase = createClient(
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!,
+        {
+          global: {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        },
+      );
 
       const { data, error } = await supabase
         .from("profiles")
