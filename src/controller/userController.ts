d5bd685d28893b0cf684bcaefe11acee9b398875
Gustavo <gustavolopez819@gmail.com>
2026-06-18 13:10:03 -0500

    feat(users): removed create client and add req user

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,36 +1,18 @@
 import { Request, Response } from "express";
 import { createClient } from "@supabase/supabase-js";
+import { supabase } from "../config/supabase";
 
 export class User {
   static getUser = async (req: Request, res: Response) => {
     try {
+      console.log(req.user);
+
       const { id } = req.params;
-      const authHeader = req.headers.authorization;
 
       if (!id) {
         res.status(400).json({ error: "Id inválido" });
         return;
       }
-
-      const token = authHeader.replace("Bearer ", "");
-
-      if (!authHeader?.startsWith("Bearer ")) {
-        return res.status(401).json({
-          error: "No autorizado",
-        });
-      }
-
-      const supabase = createClient(
-        process.env.SUPABASE_URL!,
-        process.env.SUPABASE_ANON_KEY!,
-        {
-          global: {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-        },
-      );
 
       const { data, error } = await supabase
         .from("profiles")
