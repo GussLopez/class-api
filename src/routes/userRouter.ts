658e3e097fb7ba47a38e146d2839c3df50e57ad8
Gustavo <gustavolopez819@gmail.com>
2026-06-18 13:09:36 -0500

    feat(users): add router using auth middleware

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { User } from "../controller/userController";
 
-const router = Router()
+const router = Router();
 
-export default router
+router.get("/:id", authMiddleware, User.getUser);
+
+export default router;
