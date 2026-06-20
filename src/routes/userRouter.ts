a79bb80b7c56cd3707c8324d9cec367589057ad7
Gustavo <gustavolopez819@gmail.com>
2026-06-20 11:48:51 -0500

    reafacto: deleted routes folder

--- src/routes/userRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authMiddleware } from "../middleware/authMiddleware";
-import { User } from "../controller/userController";
-
-const router = Router();
-
-router.get("/:id", authMiddleware, User.getUser);
-
-export default router;
