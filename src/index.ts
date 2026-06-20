3e39f860dd9f59e604f302519da22cb0196a27ab
Gustavo <gustavolopez819@gmail.com>
2026-06-20 11:49:06 -0500

    reafacto: deleted src folder

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import server from './server'
-import colors from 'colors'
-
-const port = process.env.PORT || 4000
-
-server.listen(port, () => {
-  console.log(colors.cyan.bold(`REST API en el puerto ${port}`));
-})
